b3cafb13bb4917453f2eb82d144368b900de56e2
medhinioak <medhinioak@gmail.com>
2020-02-18 13:51:18 +0530

    feat(GenreCard): added css for genre cards

--- src/components/GenreCard/index.jsx
+++ src/components/GenreCard/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import './index.css';
 import Button from '../Button';
 
 
 function GenreCard(props) {
   const { name, imageLink, getSongs } = props;
   return (
-    <div className="GenreCard">
+    <div className="genre-card">
       <img src={imageLink} alt="/" />
-      {/* <Link to="/songs?genre=:name"> */}
       <Button text={name} click={getSongs} />
-      {/* </Link> */}
     </div>
 
   );
