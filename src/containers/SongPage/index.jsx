18b3757449b5cd4e851e832e10da656540ff9a30
medhinioak <medhinioak@gmail.com>
2020-02-18 14:02:03 +0530

    feat(SongPage): added css for song page

--- src/containers/SongPage/index.jsx
+++ src/containers/SongPage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SongCard from '../../components/SongCard';
+import './index.css';
 
 const SongPage = (props) => {
   const { songsOfGenre, genreName } = props;
@@ -18,7 +19,7 @@ const SongPage = (props) => {
       <p>
         {genreName}
       </p>
-      <div className="Songs-container">
+      <div className="songs-container">
         {songs}
       </div>
     </div>
