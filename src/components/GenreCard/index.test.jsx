2d13391ad869ce72866045db460e3feab9d643cd
medhinioak <medhinioak@gmail.com>
2020-02-18 14:33:44 +0530

    tests(GenreCard): added tests for genre card

--- src/components/GenreCard/index.test.jsx
+++ src/components/GenreCard/index.test.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import GenreCard from './index';
 
 
-describe('the genre cards', () => {
+describe('the genre cards', (w) => {
   it('should render correctly', () => {
     const { asFragment } = render(<GenreCard />);
 
     expect(asFragment()).toMatchSnapshot();
+  });
+  it('should call get songs when the button is clicked', () => {
+    const mockGetSongs = jest.fn();
+    const { getByTestId } = render(<GenreCard getSongs={mockGetSongs} />);
+    fireEvent.click(getByTestId('test-btn'));
+    expect(mockGetSongs).toHaveBeenCalled();
   });
 });
