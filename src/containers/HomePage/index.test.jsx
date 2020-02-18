4ba1f37bd3fe6af162573690bb2f5a8ee36917e8
medhinioak <medhinioak@gmail.com>
2020-02-18 14:46:43 +0530

    fix(Homepage): fixed eslint error

--- src/containers/HomePage/index.test.jsx
+++ src/containers/HomePage/index.test.jsx
@@ -13,6 +13,7 @@ describe('the homepage container', () => {
   it('should make an api call when the button is clicked', () => {
     const mockAxios = jest.spyOn(axios, 'get');
     mockAxios.mockResolvedValue({ data: { data: { } } });
+    // eslint-disable-next-line max-len
     const { getByTestId } = render(<HomePage setIsSyncClicked={() => {}} setSongsList={() => {}} />);
 
     fireEvent.click(getByTestId('test-btn'));
