681dd168c85fa28c57e1b2739e5fe8248313e243
medhinioak <medhinioak@gmail.com>
2020-02-18 14:13:09 +0530

    fix(snapshots): updated snapshots

--- src/components/SongCard/index.test.jsx
+++ src/components/SongCard/index.test.jsx
@@ -5,7 +5,7 @@ import SongCard from './index';
 
 describe('the song cards', () => {
   it('should render correctly', () => {
-    const { asFragment } = render(<SongCard />);
+    const { asFragment } = render(<SongCard artists={[]} />);
 
     expect(asFragment()).toMatchSnapshot();
   });
